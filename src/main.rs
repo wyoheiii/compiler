84aa41b2b945e5f46fd946e0719827ca1d9f6c77
wyoheiii <seikimatu4649@gmail.com>
2023-05-24 20:43:43 +0900

    引数を加減算した値をreturnする

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,39 @@
 fn main() {
   let args: Vec<String> = std::env::args().collect();
   if args.len() != 2 {
-    println!("hikisuuga tigau");
+    eprintln!("hikisuuga tigau");
     std::process::exit(1);
   }
+  let args = args[1].chars().collect();
+  let mut i = 0;
+  let (mut val, mut size) = strtol(&args, i, 10);
+  i += size;
   println!(".intel_syntax noprefix");
   println!(".globl main");
   println!("main:");
-  println!("  mov rax, {}", args[1]);
+  println!("  mov rax, {}",  val);
+  while i < args.len(){
+    match args[i] {
+      '+' => {
+        i += 1;
+        (val, size) = strtol(&args, i, 10);
+        i += size;
+        println!("  add rax, {}", val);
+      },
+      '-' =>{
+        i += 1;
+        (val, size) = strtol(&args, i, 10);
+        i+= size;
+        println!("  sub rax, {}", val);
+      }
+      _ => {
+        eprintln!("err");
+        std::process::exit(1);
+      },
+    }
+  }
   println!("  ret");
+}
 
 fn strtol(vec:& Vec<char>, mut i:usize, base:u32) -> (i64, usize) {
   let mut res = 0;
